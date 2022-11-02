a8f2b62da89f6695d4085147197b6401503f3c3f
SuperNagibatorFunt3000 <110674014+SuperNagibatorFunt3000@users.noreply.github.com>
2022-11-02 19:18:47 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,4 @@
+-- Script GUID: {18C065A1-0149-44AE-97AD-94C604D1B7E0}
 (game:GetService("Chat")):RegisterChatCallback(Enum.ChatCallbackType.OnCreatingChatWindow, function()
     local v6 = {}
     v6.BubbleChatEnabled = true
